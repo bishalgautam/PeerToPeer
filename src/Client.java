14052cff27de0a8d2b4ba33132ff3f8466488e62
bishalgautam <bishal.htd04@gmail.com>
2016-06-16 00:47:33 -0400

    got stuck bad
    cant figure out why the try bock is not running!

--- src/Client.java
+++ src/Client.java
@@ -4,100 +4,296 @@ import java.nio.*;
 import java.nio.channels.*;
 import java.util.*;
 
-public class Client {
-	Socket requestSocket;           //socket connect to the server
-	ObjectOutputStream out;         //stream write to the socket
- 	ObjectInputStream in;          //stream read from the socket
-	                
- 
-	public void Client() {}
 
-	void run() throws NumberFormatException, ClassNotFoundException
-	{
-		try{
-			//create a socket to connect to the server
+
+public class Client implements Runnable {
+	private Socket requestSocket; // socket connect to the server
+	private ObjectOutputStream out; // stream write to the socket
+	private ObjectInputStream in; // stream read from the socket
+	protected ServerSocket serverSocket;
+	private int serverPort;
+	private List<File> file = new ArrayList<File>();
+	private List<Integer> list = new ArrayList<Integer>();
+	private Map<File, Integer> map = new HashMap<File, Integer>();
+	protected int downloadNeighbour;
+	protected int uploadNeighbour;
+	protected int listsize;
+
+	public Client(int serverPort) {
+		this.serverPort = serverPort;
+
+	}
+
+	public void openPeerServerPort() {
+		try {
+			this.serverSocket = new ServerSocket(serverPort);
+			System.out.println("Server started at " + serverPort);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException("Server not opened at :" + serverPort, e);
+		}
+
+	}
+
+	public void connectDownloadNeighbour() throws InterruptedException {
+		while (true) {
+			try {
+				System.out.println("Inside ConnectDownNeighbour");
+				System.out.println("Attempting connection to download Neighbor");
+				requestSocket = new Socket("localhost", downloadNeighbour);
+				System.out.println("Connected to downloadNeighbor" + downloadNeighbour);
+				break;
+			} catch (Exception e) {
+				Thread.sleep(2000);
+			}
+		}
+	}
+
+	public void run() {
+		
+		try {
+		// get the server's chunk
+		this.getChunkServer();
+
+		this.openPeerServerPort();
+
+		
+  
+//		try {
+//			new UploadHandler(serverSocket.accept(), serverPort).start();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	
+//    		while(true) {
+    			// accept the connection at user provided port no.
+    			try {
+					new UploadHandler(serverSocket.accept(),serverPort).start();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        
+        		/*
+        		 * **** here is the tricky part when the UploadHandler is spawned
+        		 * parallel to it the following code will also run
+        		 * 
+        		 */
+
+        		try {
+        			System.out.println("hey");
+        			this.connectDownloadNeighbour();
+        			out = new ObjectOutputStream(requestSocket.getOutputStream());
+        			out.flush();
+        			in = new ObjectInputStream(requestSocket.getInputStream());
+
+        			System.out.println((String) in.readObject());
+
+        		} catch (InterruptedException e) {
+        			// TODO Auto-generated catch block
+        			e.printStackTrace();
+        		} catch (IOException e) {
+        			// TODO Auto-generated catch block
+        			e.printStackTrace();
+        		} catch (ClassNotFoundException e) {
+        			// TODO Auto-generated catch block
+        			e.printStackTrace();
+        		}
+        	}
+
+//		}    		
+   
+	 finally {
+		try {
+			serverSocket.close();
+			in.close();
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}		
+		
+
+
+	public void getChunkServer() {
+
+		try {
+			// create a socket to connect to the server
 			requestSocket = new Socket("localhost", 8000);
 			System.out.println("Connected to localhost in port 8000");
-			//initialize inputStream and outputStream
+			// initialize inputStream and outputStream
 			out = new ObjectOutputStream(requestSocket.getOutputStream());
 			out.flush();
 			in = new ObjectInputStream(requestSocket.getInputStream());
 			InputStream input = null;
 			OutputStream output = null;
-			
-			int no = Integer.parseInt((String)in.readObject());
-			int listsize = Integer.parseInt((String)in.readObject());
-			 
-			 for(int i = no-1 ; i < listsize ; i = i+5)	{
-				 File f1 = (File)in.readObject();
-				 System.out.println("Received Chunk"+i);
-				 
-				 File f = new File("/Users/bishalgautam/Desktop/test/"+no+"/chunk"+String.format("%03d",i));
-					try {
-						
-						 input = new FileInputStream(f1);
-						 System.out.println("inside try block");
-						 output = new FileOutputStream(f);
-						byte[] buf = new byte[102400];
-						int bytesRead=0;
-						while ((bytesRead = input.read(buf)) > 0) {
-							System.out.println("inside the while");
-							output.write(buf, 0, bytesRead);}
-					} finally {
-						if(input != null)
+
+			uploadNeighbour = Integer.parseInt((String) in.readObject());
+			downloadNeighbour = Integer.parseInt((String) in.readObject());
+
+			int no = Integer.parseInt((String) in.readObject());
+			listsize = Integer.parseInt((String) in.readObject());
+			String name = (String) in.readObject();
+
+			for (int i = no - 1; i < listsize; i = i + 5) {
+				File f1 = (File) in.readObject();
+				System.out.println("Received Chunk" + i);
+
+				File f = new File(
+						"/Users/bishalgautam/Desktop/test/" + no + "/" + name + "." + String.format("%03d", i + 1));
+				try {
+
+					input = new FileInputStream(f1);
+					System.out.println("inside try block");
+					output = new FileOutputStream(f);
+					byte[] buf = new byte[102400];
+					int bytesRead = 0;
+					while ((bytesRead = input.read(buf)) > 0) {
+						// System.out.println("inside the while");
+						output.write(buf, 0, bytesRead);
+					}
+				} finally {
+					if (input != null) {
 						input.close();
-						if(output != null)
+					}
+					if (output != null) {
 						output.close();
 					}
-//					F.add(f);
-//					list.add(I);
-			 }
-			
-		}
-		catch (ConnectException e) {
-    			System.err.println("Connection refused. You need to initiate a server first.");
-		} 
-		catch ( ClassNotFoundException e ) {
-            		System.err.println("Class not found");
-        	} 
-		catch(UnknownHostException unknownHost){
-			System.err.println("You are trying to connect to an unknown host!");
-		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
-		}
-		finally{
-			//Close connections
-			try{
-				if(in != null)
-				in.close();
-				if(out != null)
-				out.close();
-				if(requestSocket != null)
-				requestSocket.close();
+				}
+				map.put(f, i);
+				file.add(f);
+				list.add(i);
 			}
-			catch(IOException ioException){
+
+		} catch (ConnectException e) {
+			System.err.println("Connection refused. You need to initiate a server first.");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Class not found");
+		} catch (UnknownHostException unknownHost) {
+			System.err.println("You are trying to connect to an unknown host!");
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} finally {
+			// Close connections
+			try {
+				if (in != null) {
+					in.close();
+				}
+				if (out != null) {
+					out.close();
+				}
+				if (requestSocket != null) {
+					requestSocket.close();
+					System.out.println("finally ended ");
+				}
+			} catch (IOException ioException) {
 				ioException.printStackTrace();
 			}
 		}
+
 	}
-	//send a message to the output stream
-	void sendMessage(String msg)
-	{
-		try{
-			//stream write the message
+
+	// send a message to the output stream
+	void sendMessage(String msg) {
+		try {
+			// stream write the message
 			out.writeObject(msg);
 			out.flush();
-		}
-		catch(IOException ioException){
+		} catch (IOException ioException) {
 			ioException.printStackTrace();
 		}
 	}
-	//main method
-	public static void main(String args[]) throws NumberFormatException, ClassNotFoundException
-	{
-		Client client = new Client();
-		client.run();
+
+	// main method
+	public static void main(String args[]) throws NumberFormatException, ClassNotFoundException {
+		System.out.println("Please specify the port number for this client to listen :");
+		Scanner in = new Scanner(System.in);
+		int port = in.nextInt();
+		in.close();
+
+		Client client = new Client(port);
+		new Thread(client).start();
+
+	}
+
+	/**
+	 * Upload handler class that enables other peer to listen to this client and
+	 * get the file list.
+	 * 
+	 */
+	private class UploadHandler extends Thread {
+		private Socket connection;
+		private ObjectInputStream in; // stream read from the socket
+		private ObjectOutputStream out; // stream write to the socket
+		private int peerID; // port no of client acting as server
+
+		public UploadHandler(Socket connection, int no) {
+			this.connection = connection;
+			this.peerID = no;
+		}
+
+		public void run() {
+
+			synchronized (this) {
+				Thread.currentThread();
+			}
+
+			try {
+				System.out.println("Handler called");
+				// initialize Input and Output streams
+				out = new ObjectOutputStream(connection.getOutputStream());
+				out.flush();
+				in = new ObjectInputStream(connection.getInputStream());
+				try {
+					sendMessage("Hi");
+
+				} catch (Exception e) {
+					throw new RuntimeException("fileList is missing", e);
+				}
+			} catch (IOException ioException) {
+				System.out.println("Disconnect with Client " + peerID);
+			} finally {
+				// Close connections
+				try {
+					if (in != null) {
+						in.close();
+					}
+					if (out != null) {
+						out.close();
+					}
+					if (connection != null) {
+						connection.close();
+					}
+				} catch (IOException ioException) {
+					System.out.println("Disconnect with Client " + peerID);
+				}
+			}
+		}
+
+		// send a message to the output stream
+		public void sendMessage(String msg) {
+			try {
+				out.writeObject(msg);
+				out.flush();
+				 System.out.println("Send message from server: " + msg + "to Client " + peerID);
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+
+		public void sendFile(File f) {
+			try {
+				out.writeObject(f);
+				out.flush();
+				// System.out.println("Send file " +fileList.indexOf(f)+ " to
+				// Client " + peerID);
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
 	}
 
 }
--- src/Server.java
+++ src/Server.java
@@ -8,9 +8,10 @@ public class Server implements Runnable{
     public static List<File> fileList = new ArrayList<File>();
 	private int sPort ; //The server will be listening on this port number
 	protected ServerSocket listener;
-	private boolean stopped;
-	protected int[][] neighbours = new int[5][2]; 
-	//public static int chunkCounter;
+	private boolean stopped = false;
+	protected static int[][] neighbours = new int[5][2]; 
+	protected static int chunkCounter;
+	protected static String name;  //name of the file
 	
 	private Server(int port)	{
 		this.sPort = port;
@@ -37,12 +38,20 @@ public class Server implements Runnable{
 	
 	public void run(){
 			// TODO Auto-generated method stub
+		openServerSocket();
 		
-//		openServerSocket();
+		try {
+			createNeighbour();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
 			     int clientNum = 1;
 			     
 	    	try {
-	        		while(true) {
+	        		while(!stopped) {
 	            		try {
 							new Handler(listener.accept(),clientNum).start();
 						} catch (IOException e) {
@@ -65,15 +74,16 @@ public class Server implements Runnable{
 	public static void main(String[] args) throws Exception {
 		
 		Server server = new Server(8000);
-		server.openServerSocket();
+//		server.openServerSocket();
         
         
 		System.out.println("The server is running."); 
 		/*
 		 * 
 		 */
-		server.createNeighbour();
+//		server.createNeighbour();
 		
+
 		System.out.println("Please enter the file from Desktop/test");
 	    // split the files into chunks of 100KB
 		Scanner readfile = new Scanner(System.in);
@@ -83,25 +93,42 @@ public class Server implements Runnable{
 		 splitFile(file );
 		 readfile.close();
 		 
-		 new Thread(server).start();	    
+		 new Thread(server).start();	 
 		 
-		// System.out.println("server is stopping");
-		// server.stop();
+		  // wait for the first chunks to be distributed to the clients
+		 try {
+//	            System.out.println("Sleeping...");
+	            Thread.sleep(30*10000);
+//	            System.out.println("Done sleeping, no interrupt.");
+	        } catch (InterruptedException e) {
+//	            System.out.println("I was interrupted!");
+	            e.printStackTrace();
+	        }
+		  
+		 //close the scanner  
+		 if (readfile != null ){
+			  readfile.close();
+		 }
+		 
+		System.out.println("server is stopping");
+	     server.stop();
        // ServerSocket listener = new ServerSocket(8000);
 		 
     	}
 	
 	public void createNeighbour() throws IOException{
 		
-		 BufferedReader in = new BufferedReader(new FileReader("Users/bishalgautam/Desktop/test/config"));
-		try{
+		 File configFile = new File("config.txt");
+		 BufferedReader in = new BufferedReader(new FileReader(configFile));
+
+		 try{
 		    String s;
 		    while((s = in.readLine()) != null){
 		    	   
 		    		String[] nextLine = s.split(" ");
 		            int clientPort = Integer.parseInt(nextLine[0]);
-		            int clientPortUn = Integer.parseInt(nextLine[0]);
-		            int clientPortDn = Integer.parseInt(nextLine[0]);
+		            int clientPortUn = Integer.parseInt(nextLine[1]);
+		            int clientPortDn = Integer.parseInt(nextLine[2]);
 		            neighbours[clientPort - 9001][0] = clientPortUn;
 		            neighbours[clientPort - 9001][1] = clientPortDn;
 		    }
@@ -109,9 +136,9 @@ public class Server implements Runnable{
 		}catch(Exception e){
 		    e.printStackTrace();
 		}finally{
-			if(in != null)
+			if(in != null){
 			  in.close();
-			
+			}
 		}
 		
 	}
@@ -120,14 +147,14 @@ public class Server implements Runnable{
 	 * split the file into the chunk of 100KB;
 	 */
 	public static void splitFile(File f) throws IOException {
-        int chunkCounter = 1; 
+        chunkCounter = 1; 
 
         int sizeOfFiles = 100*1024;// 100KB
         byte[] buffer = new byte[sizeOfFiles];
 
         try (BufferedInputStream bis = new BufferedInputStream(
                 new FileInputStream(f))) {//try-with-resources to ensure closing stream
-            String name = f.getName();
+               name = f.getName();
 
             int splitsize = 0;
             while ((splitsize = bis.read(buffer)) > 0) {
@@ -135,13 +162,17 @@ public class Server implements Runnable{
                 File newFile = new File(f.getParent(), name + "."
                         + String.format("%03d", chunkCounter++));
                 fileList.add(newFile);
+                
                 System.out.println("Files Created"+(chunkCounter-1)+":"+newFile.length());
                 
                 try (FileOutputStream out = new FileOutputStream(newFile)) {
                     out.write(buffer, 0, splitsize);//tmp is chunk size
                 }
             }
+            chunkCounter--;
+            System.out.println("no of total chunks: "+chunkCounter);
         }
+        
         
         
     }
@@ -151,33 +182,38 @@ public class Server implements Runnable{
      	* A handler thread class.  Handlers are spawned from the listening
      	* loop and are responsible for dealing with a single client's requests.
      	*/
-    	private static class Handler extends Thread {
-        private String message;    //message received from the client
-		private String MESSAGE;    //uppercase message send to the client
+    private static class Handler extends Thread {
 		private Socket connection;
         private ObjectInputStream in;	//stream read from the socket
-        private ObjectOutputStream out;    //stream write to the socket
-		private int no;		//The index number of the client
-
-        	public Handler(Socket connection, int no) {
+        private ObjectOutputStream out;  //stream write to the socket
+		private int no;		            //The index number of the client
+		
+		public Handler(Socket connection, int no) {
                 this.connection = connection;
 	    		this.no = no;
         	}
 
         public void run() {
- 		try{
+        	
+       	synchronized(this){
+       		Thread.currentThread();
+        		
+        	}
+ 		
+       	try{
+ 			
 			//initialize Input and Output streams
 			out = new ObjectOutputStream(connection.getOutputStream());
 			out.flush();
 			in = new ObjectInputStream(connection.getInputStream());
 			try{
-				
-
-					/*
-					 * 
-					 */
+     			    sendMessage(""+neighbours[no-1][0]);// sending the uploadneighbour 
+     			    
+     			    sendMessage(""+neighbours[no-1][1]);//sending the downloadneighbour
+				    
 					sendMessage(""+no);
 					sendMessage(""+fileList.size());
+					sendMessage(name); // sending the input file name
 					
 					for(int i = no-1; i < fileList.size(); i=i+5){
 						sendFile(fileList.get(i));
@@ -194,9 +230,15 @@ public class Server implements Runnable{
 		finally{
 			//Close connections
 			try{
+				if(in != null){
 				in.close();
+				}
+				if(out != null){
 				out.close();
+				}
+				if(connection != null){
 				connection.close();
+				}
 			}
 			catch(IOException ioException){
 				System.out.println("Disconnect with Client " + no);
@@ -204,9 +246,8 @@ public class Server implements Runnable{
 		}
 	}
 
-  
 	//send a message to the output stream
-	public void sendMessage(String msg)
+	public void  sendMessage(String msg)
 	{
 		try{
 			out.writeObject(msg);
@@ -222,7 +263,7 @@ public class Server implements Runnable{
 		try{
 			out.writeObject(f);
 			out.flush();
-			System.out.println("Send file " +fileList.indexOf(f)+ " to Client " + no);
+			System.out.println("Send file  from server :" +fileList.indexOf(f)+ " to Client " + no);
 		}
 		catch(IOException ioException){
 			ioException.printStackTrace();
